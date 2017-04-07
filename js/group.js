7ee091e5504f1e81410e6d0702ffa73b91026d17
looknd <looknd@vip.qq.com>
2017-04-08 01:33:46 +0800

    home

--- js/group.js
+++ js/group.js
@@ -1,6 +1,6 @@
 var btn=document.querySelector('.fork');
 var head=document.querySelector('header');
-btn.innerHTML="<strong>欢迎来到前端攻城狮城堡</strong>";
+btn.innerHTML="<strong>崇尚科学</strong>";
 btn.style.display='block';
 btn.href='https://llylife.github.io/lifeblog/';
 head.innerHTML="<h1>时代洞晰者</h1><p>深入探索WEB前沿技术，开拓JAVASCRIPT新时代</p>"
