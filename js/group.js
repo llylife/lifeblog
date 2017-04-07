015127c391cb19abd6878958d1a4d0c9bc44e5bf
looknd <looknd@vip.qq.com>
2017-04-08 00:51:09 +0800

    js up

--- js/group.js
+++ js/group.js
@@ -3,5 +3,5 @@ btn.innerHTML="<strong>欢迎来到前端攻城狮城堡</strong>";
 btn.style.display='block';
 btn.href='https://llylife.github.io/lifeblog/';
 setTimeout(function(){
-document.querySelector('footer').innerHTML="CopyRight(@)2017"
+document.querySelector('footer').innerHTML="CopyRight © 2017"
 },200)
